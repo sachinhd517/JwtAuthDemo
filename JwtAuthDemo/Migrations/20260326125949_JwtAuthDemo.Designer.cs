08ccc97762ffac523c089047ecc983ce210eb569
sachinhd517 <sachinhd517@gmail.com>
2026-03-26 18:33:20 +0530

    Rename RefreshToken table, make User.Role nullable
    Renamed RefreshToken table to RefreshTokens for consistency and updated related foreign/primary keys. Changed Users.Role column to nullable. Updated ApplicationDBContextModelSnapshot and added migration files to reflect these schema changes.

--- JwtAuthDemo/Migrations/20260326125949_JwtAuthDemo.Designer.cs
+++ JwtAuthDemo/Migrations/20260326125949_JwtAuthDemo.Designer.cs
@@ -4,6 +4,7 @@ using JwtAuthDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JwtAuthDemo.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260326125949_JwtAuthDemo")]
+    partial class JwtAuthDemo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ JwtAuthDemo/Migrations/20260326125949_JwtAuthDemo.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JwtAuthDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class JwtAuthDemo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken");
+
+            migrationBuilder.RenameTable(
+                name: "RefreshToken",
+                newName: "RefreshTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshToken_UserId",
+                table: "RefreshTokens",
+                newName: "IX_RefreshTokens_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_Users_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RefreshTokens",
+                table: "RefreshTokens");
+
+            migrationBuilder.RenameTable(
+                name: "RefreshTokens",
+                newName: "RefreshToken");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RefreshTokens_UserId",
+                table: "RefreshToken",
+                newName: "IX_RefreshToken_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RefreshToken",
+                table: "RefreshToken",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_Users_UserId",
+                table: "RefreshToken",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
